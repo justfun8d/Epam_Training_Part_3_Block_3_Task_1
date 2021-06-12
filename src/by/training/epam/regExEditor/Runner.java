bdf855bdba487541718c42d44cb0d04c3ec4d190
justfun8d <justfun8d@gmail.com>
2021-06-12 18:47:27 +0300

    First commit!

---
+++ src/by/training/epam/regExEditor/Runner.java
@@ -0,0 +1,11 @@
+package by.training.epam.regExEditor;
+
+import by.training.epam.regExEditor.controller.RegExController;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        new RegExController().run();
+    }
+}
